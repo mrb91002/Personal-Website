5ddbde6ed126976066058a9507204dbeb3b3b3f8
Matthew Benavides <matthewrbenavides@gmail.com>
2017-02-22 15:55:26 -0800

    Added coin collision detection, changed animation from javascript to css

--- index.js
+++ index.js
@@ -6,15 +6,24 @@ const coins = document.querySelector('.coins');
 const image2 = document.querySelector('.image2');
 const yoshi = document.querySelector('.yoshi');
 const yoshiContainer = document.querySelector('.yoshi-container');
+const commentBoxBottom = document.querySelector('.comment-box-bottom');
+const coinBlock1 = document.querySelector('.coin-block-1');
 
-var animating = false;
+var running = false;
+var jumping = false;
 var currentPosition = 0;
 var flipped = false;
 
+// remove all animations and reset animation status
 yoshi.addEventListener('animationend', function() {
-  yoshi.classList.toggle('animate-now');
-  yoshi.classList.toggle('sprite');
-  animating = false;
+  yoshi.classList.remove('animate-run');
+  yoshi.classList.remove('sprite-run');
+  yoshi.classList.remove('animate-jump');
+  yoshi.classList.remove('sprite-jump');
+  yoshiContainer.classList.remove('animate-jump-vert');
+  jumping = false;
+  running = false;
+  console.log(jumping, running);
 })
 
 window.addEventListener('scroll', function() {
@@ -23,19 +32,11 @@ window.addEventListener('scroll', function() {
   image2.style.left = `-${window.pageYOffset * .4}px`;
   coins.style.left = `-${window.pageYOffset * .4}px`;
 
-  if (animating === false) {
-    yoshi.classList.toggle('sprite');
-    yoshi.classList.toggle('animate-now');
-    animating = true;
+  if (running === false) {
+    yoshi.classList.toggle('sprite-run');
+    yoshi.classList.toggle('animate-run');
+    running = true;
   }
-    // yoshi.animate(yoshiRunRight, yoshiRunRightTiming);
-  // }
-
-  // var timer = function() {
-  //   setTimeout
-  // }
-
-
 
   if (currentPosition > window.pageYOffset && flipped === false) {
     yoshi.classList.toggle('flip');
@@ -47,64 +48,64 @@ window.addEventListener('scroll', function() {
     flipped = false;
   }
 
+  // if (window.pageYOffset > 1430 && window.pageYOffset < 1590 ) {
+  //   console.log('jump');
+  //   if (jumping === false) {
+  //     jumping = true;
+  //     yoshiContainer.classList.toggle('animate-jump-vert')
+  //     yoshi.classList.toggle('sprite-jump');
+  //     yoshi.classList.toggle('animate-jump');
+  //   }
+  // }
+
+  if (window.pageYOffset > 2225 && window.pageYOffset < 2475) {
+    console.log(window.pageYOffset, yoshiContainer.style );
+      yoshiContainer.style.bottom =
+        `${((window.pageYOffset - 2225) * .2 ) + 150}px`;
+  }
+  //  else {
+  //   yoshiContainer.style.bottom =
+  //     `${window.pageYOffset - 2225 + 150}px`;
+  // }
+
+  // console.log('yoshibox', yoshi);
+  console.log(window.pageYOffset, yoshi.getBoundingClientRect());
 
   currentPosition = window.pageYOffset
-
-
-  console.log('scrolling', window.pageYOffset, window.screenY);
 });
 
-const yoshiJump =
-[
-  {
-    transform: 'translate3D(-0%, -0%, 0)',
-    background: 'url("images/YoshiTest2.gif") no-repeat 0.929% 20.565%'
-  },
-  {
-    transform: 'translate3D(-0%, -50%, 0)',
-    background: 'url("images/YoshiTest2.gif") no-repeat 0.929% 20.565%',
-    offset: .25
-  },
-  {
-    transform: 'translate3D(-0%, -50%, 0)',
-    background: 'url("images/YoshiTest2.gif") no-repeat 6.574% 20.319%',
-    offset: .25
-  },
-  {
-    transform: 'translate3D(-0%, -100%, 0)',
-    background: 'url("images/YoshiTest2.gif") no-repeat 6.574% 20.319%',
-    offset: .5
-  },
-  {
-    transform: 'translate3D(-0%, -100%, 0)',
-    background: 'url("images/YoshiTest2.gif") no-repeat 12.13% 20.079%',
-    offset: .5
-  },
-  {
-    transform: 'translate3D(-0%, -0%, 0)',
-    background: 'url("images/YoshiTest2.gif") no-repeat 12.13% 20.079%',
-    offset: 1
-  },
-];
 
-const yoshiTiming = {
-  duration: 700,
-  iterations: 1,
-}
-
+// Animate Jump
 document.addEventListener('keydown', function(e) {
   if (e.keyCode == 32) {
     e.preventDefault();
-    yoshi.animate(yoshiJump, yoshiTiming);
+
+    if (jumping === false) {
+
+      setTimeout(function() {
+        console.log(window.pageYOffset);
+
+        if (window.pageYOffset > 1430 && window.pageYOffset < 1590 ) {
+          console.log('contact');
+          coinBlock1.src = "images/used-coin-block.png"
+        }
+      }, 200);
+
+      jumping = true;
+      yoshiContainer.classList.toggle('animate-jump-vert')
+      yoshi.classList.toggle('sprite-jump');
+      yoshi.classList.toggle('animate-jump');
+    }
+
+
   }
 });
 
-
 // set up text to print, each item in array is new line
 var aText = new Array(
-"Hi! I heard you want to hire mario... but he's nowhere to be found!", " ", "Perhaps you should just hire the programmer who made this world instead...", " ", "Either way, let's get going!"
+"Hi! I heard you want to hire mario... but he's nowhere to be found!", " ", "Perhaps you should just hire the programmer who made this world instead...", " ", "Either way, let's get scrolling!"
 );
-var iSpeed = 100; // time delay of print out
+var iSpeed = 60; // time delay of print out
 var iIndex = 0; // start printing array at this posision
 var iArrLength = aText[0].length; // the length of the text array
 var iScrollAt = 20; // start scrolling up at this many lines
@@ -113,27 +114,39 @@ var iTextPos = 0; // initialise text position
 var sContents = ''; // initialise contents variable
 var iRow; // initialise current row
 
-function typewriter()
-{
- sContents =  ' ';
- iRow = Math.max(0, iIndex-iScrollAt);
- var destination = document.getElementById("typedtext");
+var finish = 0;
 
- while ( iRow < iIndex ) {
-  sContents += aText[iRow++] + '<br />';
- }
- destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
- if ( iTextPos++ == iArrLength ) {
-  iTextPos = 0;
-  iIndex++;
-  if ( iIndex != aText.length ) {
-   iArrLength = aText[iIndex].length;
-   setTimeout("typewriter()", 500);
+function typewriter() {
+  sContents =  ' ';
+  iRow = Math.max(0, iIndex-iScrollAt);
+  var destination = document.getElementById("typedtext");
+
+  while ( iRow < iIndex ) {
+    sContents += aText[iRow++] + '<br />';
   }
- } else {
-  setTimeout("typewriter()", iSpeed);
- }
-}
 
+  destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+  if ( iTextPos++ == iArrLength ) {
+    iTextPos = 0;
+    iIndex++;
+
+    if ( iIndex != aText.length ) {
+      iArrLength = aText[iIndex].length;
+      setTimeout("typewriter()", 500);
+    }
+  } else {
+    setTimeout("typewriter()", iSpeed);
+  }
+
+  if (iArrLength === iTextPos) {
+    finish += 1;
+    if (finish === 5) {
+      console.log('done');
+      setTimeout(function() {
+        commentBoxBottom.style.display = 'none';
+      }, 3000)
+    }
+  }
+}
 
 typewriter();
